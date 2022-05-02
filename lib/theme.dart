8e1f53f8b7fe24fbea66e6c59221ee4428d34ea1
eunkee <eunkee@musma.net>
2022-05-02 13:30:49 +0900

    flutter ui shoppingcart

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_shoppingcart/constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primarySwatch: kPrimaryColor,
+    scaffoldBackgroundColor: kPrimaryColor,
+  );
+}
